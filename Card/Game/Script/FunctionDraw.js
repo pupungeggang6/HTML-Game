674fcaf6bbfe4b652d651dc15c64a97d2d851c60
pupungeggang6 <pupungeggang6@gmail.com>
2024-07-02 17:20:27 +0900

    Start

--- Card/Game/Script/FunctionDraw.js
+++ Card/Game/Script/FunctionDraw.js
@@ -1,5 +1,5 @@
 function drawSceneInit() {
-    context.font = '32px Opensans'
+    context.font = '32px neodgm'
     context.textAlign = 'left'
     context.textBaseline = 'top'
     context.fillStyle = 'Black'
--- Card/Game/Script/UI.js
+++ Card/Game/Script/UI.js
@@ -1,6 +1,10 @@
 let UI = {
     title : {
-        textTitle : [8, 8]
+        textTitle : [8, 8],
+        buttonStart : [160, 160, 960, 80],
+        textStart : [168, 184],
+        buttonErase : [160, 240, 960, 80],
+        textErase : [168, 264],
     },
 
     field : {
