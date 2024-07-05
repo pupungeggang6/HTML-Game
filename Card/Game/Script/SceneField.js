9614c43fb353ae9c2f84b71d4bf3dfbea7813f73
pupungeggang6 <pupungeggang6@gmail.com>
2024-07-06 02:33:09 +0900

    Start

---
+++ Card/Game/Script/SceneField.js
@@ -0,0 +1,19 @@
+function loopField() {
+    displayField()
+}
+
+function displayField() {
+    drawSceneInit()
+}
+
+function mouseUpField(x, y, button) {
+
+}
+
+function keyDownField(key) {
+
+}
+
+function keyUpField(key) {
+
+}
