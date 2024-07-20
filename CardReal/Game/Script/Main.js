8fd391a31f0ce5f2a2bdeff145c4f1d1863b98f4
pupungeggang6 <pupungeggang6@gmail.com>
2024-07-20 11:00:15 +0900

    CardReal-0.0.0:Start

---
+++ CardReal/Game/Script/Main.js
@@ -0,0 +1,83 @@
+window.onload = main
+window.onerror = errorHandle
+window.oncontextmenu = rightClick
+
+function main() {
+    canvas = document.getElementById('Screen')
+    context = canvas.getContext('2d')
+
+    canvas.addEventListener('mouseup', mouseUp, false)
+    window.addEventListener('keydown', keyDown, false)
+    window.addEventListener('keyup', keyUp, false)
+
+    loadImage()
+
+    gameFrameCurrent = Date.now()
+    gameFramePrevious = Date.now() - 16
+    delta = gameFrameCurrent - gameFramePrevious
+    gameInstance = requestAnimationFrame(loop)
+}
+
+function loop() {
+    gameFrameCurrent = Date.now()
+    delta = gameFramePrevious - gameFrameCurrent
+
+    if (scene === 'Title') {
+        loopTitle()
+    } else if (scene === 'Ready') {
+        loopReady()
+    } else if (scene === 'Game') {
+        loopGame()
+    }
+
+    gameFramePrevious = Date.now()
+}
+
+function mouseUp(event) {
+    let targetRect = canvas.getBoundingClientRect()
+    let x = event.clientX - targetRect.left
+    let y = event.clientY - targetRect.top
+    let button = event.button
+
+    if (scene === 'Title') {
+        mouseUpTitle(x, y, button)
+    } else if (scene === 'Ready') {
+        mouseUpReady(x, y, button)
+    } else if (scene === 'Game') {
+        mouseUpGame(x, y, button)
+    }
+}
+
+function keyDown(event) {
+    let key = event.key
+
+    if (scene === 'Title') {
+        keyDownTitle(key)
+    } else if (scene === 'Ready') {
+        keyDownReady(key)
+    } else if (scene === 'Game') {
+        keyDownGame(key)
+    }
+}
+
+function keyUp(event) {
+    let key = event.key
+
+    if (scene === 'Title') {
+        keyUpTitle(key)
+    } else if (scene === 'Ready') {
+        keyUpReady(key)
+    } else if (scene === 'Game') {
+        keyUpGame(key)
+    }
+}
+
+function errorHandle(err, url, line, col, obj) {
+    if (obj != null) {
+        cancelAnimationFrame(gameInstance)
+    }
+}
+
+function rightClick() {
+    return false
+}
---
+++ CardReal/Game/Script/SceneReady.js
@@ -0,0 +1,19 @@
+function loopReady() {
+    displayReady()
+}
+
+function displayReady() {
+    drawSceneInit()
+}
+
+function mouseUpReady(x, y, button) {
+
+}
+
+function keyDownReady(key) {
+
+}
+
+function keyUpReady(key) {
+
+}
